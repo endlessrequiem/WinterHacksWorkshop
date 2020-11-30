9db82e37c7dba9fc591337743bcac06a467a8090
endlessrequiem <austin.j.white.96@gmail.com>
2020-11-30 15:51:21 -0800

    okay this is the actual finalized starting point, maybe

---
+++ app/src/androidTest/java/csclub/winterhacksworkshop/ui/home/FirstFragmentTest.java
@@ -0,0 +1,29 @@
+package csclub.winterhacksworkshop.ui.home;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import csclub.winterhacksworkshop.MainActivity;
+import csclub.winterhacksworkshop.R;
+
+public class FirstFragmentTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void ButtonTest() {
+
+
+    }
+
+}
--- app/src/main/java/csclub/winterhacksworkshop/ui/home/FirstFragment.java
+++ app/src/main/java/csclub/winterhacksworkshop/ui/home/FirstFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import csclub.winterhacksworkshop.R;
@@ -15,6 +18,8 @@ import csclub.winterhacksworkshop.R;
 public class FirstFragment extends Fragment {
 
     private FirstFragmentViewModel firstFragmentViewModel;
+    Button testButton;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -22,6 +27,7 @@ public class FirstFragment extends Fragment {
                 new ViewModelProvider(this).get(FirstFragmentViewModel.class);
         View root = inflater.inflate(R.layout.fragment_first, container, false);
         final TextView textView = root.findViewById(R.id.text_firstfragment);
+
 
 
         return root;
