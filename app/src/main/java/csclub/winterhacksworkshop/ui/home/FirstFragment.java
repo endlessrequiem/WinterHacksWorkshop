f733ba51213a59d28d07578c5fcf0d74d6a2a406
endlessrequiem <austin.j.white.96@gmail.com>
2020-11-30 15:07:25 -0800

    added in content, not yet finalized

---
+++ app/src/main/java/csclub/winterhacksworkshop/ui/home/FirstFragment.java
@@ -0,0 +1,47 @@
+package csclub.winterhacksworkshop.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import csclub.winterhacksworkshop.R;
+
+public class FirstFragment extends Fragment {
+
+    private FirstFragmentViewModel firstFragmentViewModel;
+    Button testButton;
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        firstFragmentViewModel =
+                new ViewModelProvider(this).get(FirstFragmentViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_first, container, false);
+        final TextView textView = root.findViewById(R.id.text_firstfragment);
+
+        testButton = root.findViewById(R.id.buttonTest);
+
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firstFragmentViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+                    @Override
+                    public void onChanged(@Nullable String s) {
+                        textView.setText(s);
+                    }
+                });
+            }
+        });
+
+        return root;
+    }
+}
