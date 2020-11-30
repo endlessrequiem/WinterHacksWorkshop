00d647968209006630d861c97a53f185cc619979
endlessrequiem <austin.j.white.96@gmail.com>
2020-11-30 14:21:32 -0800

    added Kotlin fragment and Kotlin support

---
+++ app/src/main/java/csclub/winterhacksworkshop/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,23 @@
+package csclub.winterhacksworkshop.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import csclub.winterhacksworkshop.R
+
+class DashboardFragment : Fragment() {
+    private var dashboardViewModel: DashboardViewModel? = null
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        dashboardViewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val textView = root.findViewById<TextView>(R.id.text_dashboard)
+        dashboardViewModel!!.text.observe(viewLifecycleOwner, Observer { s -> textView.text = s })
+        return root
+    }
+}
