3597b26156554b8d767bbabda95569f448746854
endlessrequiem <austin.j.white.96@gmail.com>
2020-11-30 15:18:13 -0800

    okay this is the finalized beginning point, i think

--- app/src/main/java/csclub/winterhacksworkshop/ui/dashboard/SecondFragment.kt
+++ app/src/main/java/csclub/winterhacksworkshop/ui/dashboard/SecondFragment.kt
@@ -4,17 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import csclub.winterhacksworkshop.R
 
-class DashboardFragment : Fragment() {
-    private var dashboardViewModel: DashboardViewModel? = null
+class SecondFragment : Fragment() {
+    private var secondFragmentViewModel: SecondFragmentViewModel? = null
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        dashboardViewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
+        secondFragmentViewModel = ViewModelProvider(this).get(SecondFragmentViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_second, container, false)
 
         return root
--- app/src/main/java/csclub/winterhacksworkshop/ui/home/FirstFragment.java
+++ app/src/main/java/csclub/winterhacksworkshop/ui/home/FirstFragment.java
@@ -4,27 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import csclub.winterhacksworkshop.R;
 
-public class HomeFragment extends Fragment {
+public class FirstFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private FirstFragmentViewModel firstFragmentViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        firstFragmentViewModel =
+                new ViewModelProvider(this).get(FirstFragmentViewModel.class);
         View root = inflater.inflate(R.layout.fragment_first, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        final TextView textView = root.findViewById(R.id.text_firstfragment);
 
 
         return root;
